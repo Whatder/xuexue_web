c1ee1f0a7c12af7f533511b25326f57ca6097e23
何何何 <523035216@qq.com>
2018-03-27 13:16:28 +0800

    修改计划状态

--- src/main/java/controller/PlanController.java
+++ src/main/java/controller/PlanController.java
@@ -4,6 +4,7 @@ import model.Plan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import response.ResponseData;
 import response.ResponseDataUtils;
@@ -32,4 +33,34 @@ public class PlanController {
         return responseData;
     }
 
+    @RequestMapping(value = "/plan/add", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseData addPlan(HttpServletRequest request) {
+        int user_id = Integer.parseInt(request.getParameter("user_id"));
+        String title = request.getParameter("title");
+        String content = request.getParameter("content");
+        int create_time = Integer.parseInt(request.getParameter("create_time"));
+        String status = request.getParameter("status");
+
+        if (planService.addPlan(user_id, title, content, create_time, status))
+            responseData = new ResponseDataUtils<String>().dataBuilder(true, "", "插入成功");
+        else {
+            responseData = new ResponseDataUtils<String>().dataBuilder(false, "插入失败", "");
+        }
+        return responseData;
+    }
+
+    @RequestMapping(value = "/plan/update", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseData updatePlan(HttpServletRequest request) {
+        String status = request.getParameter("status");
+        int id = Integer.parseInt(request.getParameter("id"));
+
+        if (planService.updateStatus(status, id))
+            responseData = new ResponseDataUtils<String>().dataBuilder(true, "", "更新成功");
+        else {
+            responseData = new ResponseDataUtils<String>().dataBuilder(false, "更新失败", "");
+        }
+        return responseData;
+    }
 }
