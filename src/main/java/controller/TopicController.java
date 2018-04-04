859ebdf748e2eb06415b575b9f0678bc4b5b221d
何何何 <523035216@qq.com>
2018-04-04 22:52:45 +0800

    删除话题修改

--- src/main/java/controller/TopicController.java
+++ src/main/java/controller/TopicController.java
@@ -61,8 +61,8 @@ public class TopicController {
     public ResponseData delTopicById(HttpServletRequest request) {
         int id = Integer.parseInt(request.getParameter("id"));
 //        删除指定topic下的评论后 再删除本身
-        if (topicService.delReplyByTopicID(topicService.getTopicByID(id).getId()) &&
-                topicService.delTopicByID(id))
+        topicService.delReplyByTopicID(topicService.getTopicByID(id).getId());
+        if (topicService.delTopicByID(id))
             responseData = new ResponseDataUtils<String>().dataBuilder(true, "", "删除成功");
         else
             responseData = new ResponseDataUtils<String>().dataBuilder(false, "删除失败", "");
